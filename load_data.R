0f1218582ec7cdd07de79d0fe429e0ac4b03b16e
Elijah Ozhmegov <ElijahOzhmegov@gmail.com>
2020-05-23 15:32:52 +0200

    add general function for loading data tables

--- load_data.R
+++ load_data.R
@@ -1,10 +1,18 @@
+# Some useful functions --------------------------------------------------------
+get_df <- function(path){
+  if (!is.numeric(path)){
+    df = read.csv2(path, sep = ',')
+  }else{
+    stop("Path variable must be a string!")
+  }
+}
 
-get_math_df <- function(){
-  math_df = read.csv2("data/student-mat.csv", sep = ',')
+get_math_df <- function(path=NULL){
+  df <- get_df("data/student-mat.csv")
 }
 
 get_port_df <- function(){
-  port_df = read.csv2("data/student-por.csv", sep = ',')
+  df <- get_df("data/student-por.csv")
 }
 
 get_full_df <- function(){
@@ -12,11 +20,13 @@ get_full_df <- function(){
   chunk2 <- get_port_df()
   
   full_df = merge(chunk1, chunk2,
-             by=c("school","sex","age","address","famsize","Pstatus","Medu",
-                  "Fedu","Mjob","Fjob","reason","nursery","internet"),
-             suffixes = c(".math",".port")
-             )
+                  by=c("school","sex","age","address","famsize","Pstatus","Medu",
+                      "Fedu","Mjob","Fjob","reason","nursery","internet"),
+                  suffixes = c(".math",".port")
+                 )
 }
+
+# loading all data -------------------------------------------------------------
 
 math_df <- get_math_df()
 port_df <- get_port_df()
