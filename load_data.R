ad4bf59e632f3476fbe3b37d689457b54a8faf08
Elijah Ozhmegov <ElijahOzhmegov@gmail.com>
2020-05-23 13:46:05 +0200

    add func for loading data

---
+++ load_data.R
@@ -0,0 +1,23 @@
+
+get_math_df <- function(){
+  math_df = read.csv2("student-mat.csv", sep = ',')
+}
+
+get_port_df <- function(){
+  port_df = read.csv2("student-por.csv", sep = ',')
+}
+
+get_full_df <- function(){
+  chunk1 <- get_math_df()
+  chunk2 <- get_port_df()
+  
+  full_df = merge(chunk1, chunk2,
+             by=c("school","sex","age","address","famsize","Pstatus","Medu",
+                  "Fedu","Mjob","Fjob","reason","nursery","internet"),
+             suffixes = c(".math",".port")
+             )
+}
+
+math_df <- get_math_df()
+port_df <- get_math_df()
+full_df <- get_math_df()
--- main.R
+++ main.R
@@ -1,14 +1,1 @@
-chunk1 = read.csv2("student-mat.csv", sep = ',')
-chunk2 = read.csv2("student-por.csv", sep = ',')
-
-str(chunk1)
-str(chunk2)
-head(chunk1[1:3,])
-
-df = merge(chunk1, chunk2,
-           by=c("school","sex","age","address","famsize","Pstatus","Medu",
-                "Fedu","Mjob","Fjob","reason","nursery","internet")
-           )
-print(nrow(df)) # 382 students (doubled students)
-
-pairs(df[27:33])
+## soon 
